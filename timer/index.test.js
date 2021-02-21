4b26047a6b11fba54f4a44b5f977a3fdbdfb3545
Yijie Ruan <yiruan@ucsd.edu>
2021-02-21 15:24:47 +0800

    updated index.test.js

--- timer/index.test.js
+++ timer/index.test.js
@@ -16,6 +16,7 @@ beforeEach(() => {
 jest.useFakeTimers()
 update()
 
+//work to rest
 test('test for update function, see if state changes from work to rest, correct time set after 25 min', () => {
   //25 minutes
   jest.advanceTimersByTime(1501000)
@@ -32,9 +33,37 @@ test('test for update function, 5 min', () => {
   expect(pomo.count).toBe(0)
   expect(pomo.minutes).toBe(20)
   expect(pomo.seconds).toBe(0)
-  jest.clearAllMocks()
+  //jest.clearAllMocks()
+})
+// state changes, work to rest, rest to work, work to long rest, long rest to work, 
+// make sure updateDom() and setProgress() is called in the right set of times.
+
+//rest to work
+test('test for update function, see if state changes from rest back to work, correct time set should be 25 mins for work', ()=>{
+  jest.advanceTimersByTime(1802000)
+  expect(pomo.minutes).toBe(25)
+  expect(pomo.seconds).toBe(0)
+  expect(pomo.count).toBe(1)
+  expect(pomo.state).toBe(statesArray[1])
 })
 
+//work to long rest
+test('test for update function, see if state changes from work to long rest, correct time set should be 15 mins for long break', ()=>{
+  jest.advanceTimersByTime(6907000)
+  expect(pomo.minutes).toBe(15)
+  expect(pomo.seconds).toBe(0)
+  expect(pomo.count).toBe(4)
+  expect(pomo.state).toBe(statesArray[3])
+})
+
+//long rest to work
+test('test for update function, see if state changes from long rest to work, correct time set should be 25 mins for work', ()=>{
+  jest.advanceTimersByTime(7808000)
+  expect(pomo.minutes).toBe(25)
+  expect(pomo.seconds).toBe(0)
+  expect(pomo.count).toBe(4)
+  expect(pomo.state).toBe(statesArray[1])
+})
 
 test('convert single digit to double digit', () => {
   expect(doubleDigit(1)).toBe('01')
