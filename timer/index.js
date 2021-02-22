27182043dda1eecd645b8cf688c938729499863a
calee0316 <calee@ucsd.edu>
2021-02-22 11:36:42 +0900

    moved all DOM-related to DOM.js, fixed testing issues

--- timer/index.js
+++ timer/index.js
@@ -1,7 +1,3 @@
-window.onload = () => {
-  pomo.init()
-}
-
 const workMinutes = 25
 const workSeconds = 0
 const restMinutes = 5
@@ -53,10 +49,10 @@ const pomo = {
   circle: null,
   radius: null,
   circumference: null,
-  test: false,
-  init: function() {
+  perc: 0,
+  init: function(initDOM, updateDOM) {
     initDOM()
-    update()
+    update(updateDOM)
   }
 }
 
@@ -65,7 +61,7 @@ const pomo = {
  * Calls every second to update states and attributes
  * of the pomo object, which eventually updates the DOM
  */
-function update() {
+function update(updateDOM) {
   setInterval(() => {
     let minutes = workMinutes
     let seconds = workSeconds
@@ -102,7 +98,7 @@ function update() {
               audio = oneUp
               audio.play()
             }
-          } else if (pomo.state === statesArray[2] || pomo.state === statesArray[3]) {
+          } else {
             // currently rest ended
             pomo.minutes = workMinutes
             pomo.seconds = workSeconds
@@ -110,125 +106,25 @@ function update() {
             audio = powerDown
             audio.play()
           }
-          setProgress(0)
+          pomo.perc = 0
         } else {
           pomo.seconds = 59
           pomo.minutes--
-          let perc = ((((minutes * 60 + seconds) - (pomo.minutes * 60 + pomo.seconds)) / (minutes * 60 + seconds)) * 100)
-          setProgress(perc)
+          pomo.perc = ((((minutes * 60 + seconds) - (pomo.minutes * 60 + pomo.seconds)) / (minutes * 60 + seconds)) * 100)
         }
       } else {
         pomo.seconds--
-        let perc = ((((minutes * 60 + seconds) - (pomo.minutes * 60 + pomo.seconds)) / (minutes * 60 + seconds)) * 100)
-        setProgress(perc)
+        pomo.perc = ((((minutes * 60 + seconds) - (pomo.minutes * 60 + pomo.seconds)) / (minutes * 60 + seconds)) * 100)
       }
+    } else {
+      pomo.perc = 0
+      pomo.minutes = workMinutes
+      pomo.seconds = workSeconds
+      pomo.count = 0
+      pomo.state = statesArray[0]
     }
     updateDOM()
   }, 1000)
-}
-
-
-function setProgress(percent) {
-  if (!pomo.test) {
-    const offset = pomo.circumference - (percent / 100) * pomo.circumference;
-    pomo.circle.style.strokeDashoffset = offset;
-  }
-}
-
-/**
- * updates the DOM based on changes that the
- * update() function made to the pomo object
- * changes will be reflected on the browser page
- */
-function updateDOM() {
-  if (!pomo.test) {
-    pomo.timeDom.textContent = doubleDigit(pomo.minutes) + ':' + doubleDigit(pomo.seconds)
-    pomo.stateDom.textContent = pomo.state
-    pomo.numPomoDom.textContent = pomo.count
-    if (pomo.state === statesArray[0] || pomo.state === statesArray[1]) {
-      pomo.picDom.href.baseVal = './img/1.png'
-    } else if (pomo.state === statesArray[2]) {
-      pomo.picDom.href.baseVal = './img/2.png'
-    } else if (pomo.state === statesArray[3]) {
-      pomo.picDom.href.baseVal = './img/3.png'
-    }
-  }
-}
-
-function initDOM() {
-  pomo.timeDom = document.getElementById('time')
-  pomo.stateDom = document.getElementById('state')
-  pomo.numPomoDom = document.getElementById('count')
-  pomo.picDom = document.getElementById('pic')
-  pomo.circle = document.querySelector('.progress-ring__circle')
-  pomo.radius = pomo.circle.r.baseVal.value;
-  pomo.circumference = pomo.radius * 2 * Math.PI;
-  pomo.circle.style.strokeDasharray = `${pomo.circumference} ${pomo.circumference}`;
-  pomo.circle.style.strokeDashoffset = `${pomo.circumference}`;
-
-  document.getElementById('button').addEventListener('click', () => {
-    if (!pomo.started) {
-      pomo.state = statesArray[1]
-      pomo.minutes = workMinutes
-      pomo.seconds = workSeconds
-      pomo.started = true
-      document.getElementById('button').textContent = buttonsArray[1]
-      document.getElementById('button').style.backgroundColor = 'red'
-      audio = oof
-      audio.play()
-    } else {
-      pomo.minutes = workMinutes
-      pomo.seconds = workSeconds
-      pomo.started = false
-      pomo.state = statesArray[0]
-      pomo.count = 0
-      document.getElementById('button').textContent = buttonsArray[0]
-      document.getElementById('button').style.backgroundColor = '#f6b432'
-      audio = gameOver
-      audio.play()
-      setProgress(0)
-    }
-  })
-
-  document.getElementById("language-picker-select").onchange = function() {
-    if (document.getElementById("language-picker-select").value === 'chinese') {
-      statesArray = states.CH
-      buttonsArray = buttons.CH
-      document.getElementById('about').textContent = '关于我们'
-      document.getElementById('lang_label').textContent = '语言：'
-      pomoCompleted = ' 帕玛多拉 完成'
-    } else if (document.getElementById("language-picker-select").value === 'korean') {
-      statesArray = states.KR
-      buttonsArray = buttons.KR
-      document.getElementById('about').textContent = '팀 소개'
-      document.getElementById('lang_label').textContent = '언어: '
-      pomoCompleted = ' 완료된 포모'
-    } else if (document.getElementById("language-picker-select").value === 'japanese') {
-      statesArray = states.JP
-      buttonsArray = buttons.JP
-      document.getElementById('about').textContent = 'わたしたち'
-      document.getElementById('lang_label').textContent = '言語: '
-      pomoCompleted = ' ポモス 完成'
-    } else if (document.getElementById("language-picker-select").value === 'english') {
-      statesArray = states.def
-      buttonsArray = buttons.def
-      document.getElementById('about').textContent = 'About us'
-      document.getElementById('lang_label').textContent = 'Language: '
-      pomoCompleted = ' pomos completed'
-    }
-    document.getElementById('button').textContent = buttonsArray[0]
-    document.getElementById('message').textContent = pomoCompleted
-    pomo.state = statesArray[0]
-    pomo.started = false
-    pomo.minutes = workMinutes
-    pomo.seconds = workSeconds
-    pomo.count = 0;
-    updateDOM()
-    setProgress(0)
-    if (document.getElementById('button').style.backgroundColor == 'red') {
-      document.getElementById('button').style.backgroundColor = '#f6b432'
-    }
-  }
 }
 
 /**
--- timer/index.test.js
+++ timer/index.test.js
@@ -1,4 +1,15 @@
-let {pomo, update, updateDOM, setProgress, initDOM,workMinutes, workSeconds, doubleDigit, statesArray} = require('./index')
+let {
+  pomo,
+  update,
+  workMinutes,
+  workSeconds,
+  restMinutes,
+  restSeconds,
+  lRestSeconds,
+  lRestMinutes,
+  doubleDigit,
+  statesArray
+} = require('./index')
 
 window.HTMLMediaElement.prototype.play = () => {
   /* do nothing */
@@ -11,10 +22,12 @@ beforeEach(() => {
   pomo.minutes = workMinutes
   pomo.seconds = workSeconds
   pomo.count = 0
+  updateDOM.mock.calls.length = 0
 })
-
+const updateDOM = jest.fn()
+const initDOM = jest.fn()
 jest.useFakeTimers()
-update()
+pomo.init(initDOM, updateDOM)
 
 //work to rest
 test('test for update function, see if state changes from work to rest, correct time set after 25 min', () => {
@@ -24,6 +37,8 @@ test('test for update function, see if state changes from work to rest, correct 
   expect(pomo.seconds).toBe(0)
   expect(pomo.count).toBe(1)
   expect(pomo.state).toBe(statesArray[2])
+  expect(pomo.perc).toBe(0)
+  expect(updateDOM.mock.calls.length).toBe(1501)
 })
 
 test('test for update function, 5 min', () => {
@@ -33,37 +48,57 @@ test('test for update function, 5 min', () => {
   expect(pomo.count).toBe(0)
   expect(pomo.minutes).toBe(20)
   expect(pomo.seconds).toBe(0)
-  //jest.clearAllMocks()
+  expect(pomo.perc).toBe(20)
+  expect(updateDOM.mock.calls.length).toBe(300)
 })
-// state changes, work to rest, rest to work, work to long rest, long rest to work, 
+// state changes, work to rest, rest to work, work to long rest, long rest to work,
 // make sure updateDom() and setProgress() is called in the right set of times.
 
 //rest to work
-test('test for update function, see if state changes from rest back to work, correct time set should be 25 mins for work', ()=>{
+test('test for update function, see if state changes from rest back to work, correct time set should be 25 mins for work', () => {
   jest.advanceTimersByTime(1802000)
   expect(pomo.minutes).toBe(25)
   expect(pomo.seconds).toBe(0)
   expect(pomo.count).toBe(1)
   expect(pomo.state).toBe(statesArray[1])
+  expect(pomo.perc).toBe(0)
+  expect(updateDOM.mock.calls.length).toBe(1802)
 })
 
 //work to long rest
-test('test for update function, see if state changes from work to long rest, correct time set should be 15 mins for long break', ()=>{
+test('test for update function, see if state changes from work to long rest, correct time set should be 15 mins for long break', () => {
   jest.advanceTimersByTime(6907000)
   expect(pomo.minutes).toBe(15)
   expect(pomo.seconds).toBe(0)
   expect(pomo.count).toBe(4)
   expect(pomo.state).toBe(statesArray[3])
+  expect(pomo.perc).toBe(0)
+  expect(updateDOM.mock.calls.length).toBe(6907)
 })
 
 //long rest to work
-test('test for update function, see if state changes from long rest to work, correct time set should be 25 mins for work', ()=>{
+test('test for update function, see if state changes from long rest to work, correct time set should be 25 mins for work', () => {
   jest.advanceTimersByTime(7808000)
   expect(pomo.minutes).toBe(25)
   expect(pomo.seconds).toBe(0)
   expect(pomo.count).toBe(4)
   expect(pomo.state).toBe(statesArray[1])
+  expect(pomo.perc).toBe(0)
+  expect(updateDOM.mock.calls.length).toBe(7808)
 })
+
+//not started
+test('test for when timer is not started', () => {
+  pomo.started = false
+  jest.advanceTimersByTime(10000)
+  expect(pomo.minutes).toBe(25)
+  expect(pomo.seconds).toBe(0)
+  expect(pomo.count).toBe(0)
+  expect(pomo.state).toBe(statesArray[0])
+  expect(pomo.perc).toBe(0)
+  expect(updateDOM.mock.calls.length).toBe(10)
+})
+
 
 // //updateDOM() time
 // test('check if the updateDom() runs correct amount of time',()=>{
