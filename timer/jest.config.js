7408356c3ed20b9bc10ffedc0248491a7ac478c0
calee0316 <calee@ucsd.edu>
2021-02-25 15:16:04 +0900

    update

--- timer/jest.config.js
+++ timer/jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   // Your normal jest config settings
   testPathIgnorePatterns: ['<rootDir>/cypress/'],
   testEnvironment: 'jsdom',
-  collectCoverage: true
+  collectCoverage: true,
+  coverageDirectory: 'jest-coverage',
 }
